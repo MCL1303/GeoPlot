553dd93545715495d5ea4ea9273cd394c6efa920
pinbraerts <shirshov.dima@gmail.com>
2017-10-19 09:23:07 +0300

    Full extract
    Not fully rewrote extract in both bash and batch

--- extract/extract.bat
+++ extract/extract.bat
@@ -1,3 +1,46 @@
 @echo off
 
-tomitaparser config.proto < ..\test\data\input\%1 > ..\test\data\intermediate_output\%~n1.xml
+set all=0
+set norm=0
+
+:while
+if "%1" neq "" (
+    if "%1" equ "-a" ( set all=1
+    ) else (
+        if "%1" equ "-n" ( set norm=1
+        ) else (
+            if "%1" neq "-na" if "%1" neq "-an" ( break
+            ) else (
+                set norm=1
+                set all=1
+            )
+        )
+    )
+    shift
+    goto :while
+)
+
+set cmd=
+
+for %%i in (tomitaparser,tomita-win32,tomita-win64) do (
+    where /q %%i && (
+        set cmd=%%i
+        break
+    )
+)
+
+if "%cmd%" equ "" (
+    echo File or tomita parser not found
+    exit 1
+)
+
+set range=
+if %all% equ 1 ( set range=(*)
+) else ( set range=(%1) )
+
+for %%i in %range% do
+    %cmd% config.proto < ..\test\data\input\%%i > ..\test\data\intermediate_output\%%~ni.xml
+
+if %norm% equ 1
+    for %%i in %range% do
+        python ..\python\normalize.py ..\test\data\intermediate_output\%%~ni.xml
