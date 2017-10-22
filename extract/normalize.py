a71022c0029d79f93b517775bb8fe5553c85f992
pinbraerts <shirshov.dima@gmail.com>
2017-10-22 20:26:48 +0300

    Full extract
    Написал программу на python, потому что много переписывать на bash и
    batch не хочется. Заодно и выудил все факты.

--- extract/normalize.py
+++ extract/normalize.py
@@ -1,15 +1,21 @@
 import xml.etree.ElementTree as ET
 import sys
-from os.path import basename as bn, splitext as sp, dirname as dn, realpath as rp, join
 
 tree = ET.parse(sys.argv[1])
 
-tree._setroot(tree.getroot().find("document").find("facts"))
+print("Normalizing file", sys.argv[1])
+doc = tree.getroot().find("document")
+if doc is None:
+	tree._setroot(ET.Element("facts"))
+else:
+	tree._setroot(doc.find("facts"))
 
-for i in tree.getroot():
-    attribs = [(j.tag, j.get("val")) for j in i]
-    i.clear()
-    for k, j in attribs:
-        i.set(k, j)
+	for i in tree.getroot():
+	    attribs = [(j.tag, j.get("val")) for j in i]
+	    i.clear()
+	    for k, j in attribs:
+	        i.set(k, j)
 
 tree.write(sys.argv[2], 'unicode', True)
+print("Normalized file", sys.argv[1])
+print()
