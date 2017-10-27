a2afd307ff371437de19e1ce3ebc3e0ed2d7bcf9
pinbraerts <shirshov.dima@gmail.com>
2017-10-27 09:56:36 +0300

    Сделал соответствующие изменения.

--- extract/normalize.py
+++ extract/normalize.py
@@ -1,13 +1,17 @@
 from xml.dom import minidom
 import sys
 
-root = minidom.parse(sys.stdin.buffer)
-outroot = minidom.getDOMImplementation().createDocument(None, 'facts', None)
-if root.documentElement.hasChildNodes():
-    for node in root.documentElement.firstChild.firstChild.childNodes:
-        newnode = outroot.createElement(node.tagName)
-        for subnode in node.childNodes:
-            newnode.setAttribute(subnode.tagName, subnode.attributes["val"].value)
-        outroot.documentElement.appendChild(newnode)
+def main():
+    root = minidom.parse(sys.stdin.buffer)
+    outroot = minidom.getDOMImplementation().createDocument(None, 'facts', None)
+    if root.documentElement.hasChildNodes():
+        for node in root.documentElement.firstChild.firstChild.childNodes:
+            newnode = outroot.createElement(node.tagName)
+            for subnode in node.childNodes:
+                newnode.setAttribute(subnode.tagName, subnode.attributes["val"].value)
+            outroot.documentElement.appendChild(newnode)
 
-outroot.writexml(sys.stdout, addindent='  ', newl='\n', encoding="UTF-8")
+    outroot.writexml(sys.stdout, addindent='  ', newl='\n', encoding="UTF-8")
+
+if __name__ == '__main__':
+    main()
