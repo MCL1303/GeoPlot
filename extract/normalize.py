c85d6b7c81c931fd1a773c6d68dc25401ad74bc6
Dmitry <shirshov.dima@gmail.com>
2017-10-27 18:38:03 +0300

    Автоматический поиск Томита-парсера на разных ОС и нормализация вывода

---
+++ extract/normalize.py
@@ -0,0 +1,22 @@
+from xml.dom import minidom
+import sys
+
+
+def main():
+    doc = minidom.parse(sys.stdin.buffer)
+    outdoc = minidom.getDOMImplementation().createDocument(None, 'facts', None)
+    if doc.documentElement.hasChildNodes():
+        for node in doc.documentElement.
+        firstChild.firstChild.childNodes:
+            newnode = outdoc.createElement(node.tagName)
+            for subnode in node.childNodes:
+                newnode.setAttribute(
+                    subnode.tagName,
+                    subnode.attributes["val"].value
+                )
+            outdoc.documentElement.appendChild(newnode)
+
+    outdoc.writexml(sys.stdout, addindent='  ', newl='\n', encoding="UTF-8")
+
+if __name__ == '__main__':
+    main()
