034b1ba5674240b30900c6a6a87d1a12648c71a5
pinbraerts <shirshov.dima@gmail.com>
2017-10-29 21:36:12 +0300

    UPD: install with export

--- test/install.sh
+++ test/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eux -o pipefail
-pwd
 wget http://download.cdn.yandex.net/tomita/tomita-linux64.bz2 -O tomita-linux64.bz2
 bunzip2 tomita-linux64.bz2
 chmod +x tomita-linux64
-PATH="$PATH;`pwd`"
+export PATH="$PATH;`pwd`"
