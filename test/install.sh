ba60783705dadcfe61646debad03570a8f5fa705
pinbraerts <shirshov.dima@gmail.com>
2017-10-29 20:28:28 +0300

    add verbose

--- test/install.sh
+++ test/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 wget http://download.cdn.yandex.net/tomita/tomita-linux64.bz2 -O /tmp/tomita.bz2
-tar -xj /tmp/tomita.bz2 /usr/bin/
+tar -xvj /tmp/tomita.bz2 /usr/bin/
 chmod +x /usr/bin/tomita-linux64
