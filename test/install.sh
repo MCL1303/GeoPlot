62c267f6b17f08674beb732562def303849444ed
pinbraerts <shirshov.dima@gmail.com>
2017-10-29 20:47:05 +0300

    UPD: config and install

--- test/install.sh
+++ test/install.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env sh
+#!/bin/bash
+set -eux -o pipefail
 
-wget http://download.cdn.yandex.net/tomita/tomita-linux64.bz2 -O /tmp/tomita.bz2
-bunzip2 /tmp/tomita.bz2 /usr/bin/
+wget http://download.cdn.yandex.net/tomita/tomita-linux64.bz2 -O /usr/bin/tomita-linux64.bz2
+bunzip2 /usr/bin/tomita-linux64.bz2
 chmod +x /usr/bin/tomita-linux64
