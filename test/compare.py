a6f73e2f03e63b334d1db98e840c584d02120e54
pinbraerts <shirshov.dima@gmail.com>
2017-10-29 14:39:16 +0300

    Переделал для всех файлов в каталоге

--- test/compare.py
+++ test/compare.py
@@ -9,21 +9,15 @@ import os.path
 
 def main():
     program_dir = os.path.dirname(os.path.realpath(__file__))
-    files = ' '.join(
-        i for i in iglob(os.path.join(program_dir, 'data', 'facts', '*'))
-        if os.path.splitext(i)[-1] == '.xml'
-    )
+    os.chdir(program_dir)
 
     subprocess.check_call([
-        os.path.join(program_dir, '..', 'extract', 'extract'),
-        files, '2>',
-        ('nul' if sys.platform.startswith('win') else '/dev/null')
-    ])
+        os.path.join('..', 'extract', 'extract'), '*'
+    ], stdout=subprocess.DEVNULL)
 
     try:
-        subprocess.check_call(["git", "diff", "--no-path", "--exit-code", files]):
+        subprocess.check_call(["git", "diff", "--no-path", "--exit-code", "data/facts"]):
     except subprocess.CalledProcessError:
-        os.chdir(program_dir)
         subprocess.call(['git', 'checkout', 'data/facts/'])
         exit(1)
 
