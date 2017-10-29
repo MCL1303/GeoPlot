18553d8fef3818881a8e387f07f716f9d0504997
pinbraerts <shirshov.dima@gmail.com>
2017-10-29 20:31:00 +0300

    bunzip2

--- test/install.sh
+++ test/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 wget http://download.cdn.yandex.net/tomita/tomita-linux64.bz2 -O /tmp/tomita.bz2
-tar -xvj /tmp/tomita.bz2 /usr/bin/
+bunzip2 /tmp/tomita.bz2 /usr/bin/
 chmod +x /usr/bin/tomita-linux64
