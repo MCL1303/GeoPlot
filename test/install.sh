88505ccbbf6b997adb4e1ce51e9a025b9828f8a9
pinbraerts <shirshov.dima@gmail.com>
2017-10-29 21:52:40 +0300

    UPD: moved to .yml

--- test/install.sh
+++ test/install.sh
@@ -3,4 +3,3 @@ set -eux -o pipefail
 wget http://download.cdn.yandex.net/tomita/tomita-linux64.bz2 -O tomita-linux64.bz2
 bunzip2 tomita-linux64.bz2
 chmod +x tomita-linux64
-export PATH="$PATH:`pwd`"
