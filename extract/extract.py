4af23d8cd45019344aa8833d792335c72b500fd0
pinbraerts <shirshov.dima@gmail.com>
2017-10-29 13:19:12 +0300

    Исправил

--- extract/extract.py
+++ extract/extract.py
@@ -28,7 +28,6 @@ def main():
         exit(1)
 
     program_dir = os.path.dirname(os.path.realpath(__file__))
-    working_dir = os.getcwd()
     os.chdir(program_dir)
     mpath = os.path.join(program_dir, "..", "test", "data")
 
@@ -57,12 +56,10 @@ def main():
             exit(4)
 
     for f in range_of_files:
-        sys.stderr.write("Processing " + f + " file\n")
+        print("Processing", f, "file", file=sys.stderr)
         bnf = os.path.splitext(os.path.basename(f))[0]
         os.system(ext_cmd.format(bnf))
-        sys.stderr.write("Processed " + f + " file\n\n")
-
-    os.chdir(working_dir)
+        print("Processed", f, "file", end="\n\n", file=sys.stderr)
 
 if __name__ == '__main__':
     main()
