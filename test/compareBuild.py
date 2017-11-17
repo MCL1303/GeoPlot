a2b94cbc938759c4e7e92acb0b01f35666b2c675
pinbraerts <shirshov.dima@gmail.com>
2017-11-17 20:45:08 +0300

    Добавил тест и изменил конфиг

---
+++ test/compareBuild.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+import subprocess
+import os
+import os.path
+from glob import iglob, glob
+
+
+def main():
+    program_dir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(program_dir)
+
+    for i in iglob(os.path.join('data', 'facts', '*.xml')):
+        print(i)
+        subprocess.check_call([os.path.join('..', 'model', 'build.bat'), i])
+    print(glob(os.path.join('data', 'facts', '*.xml')))
+
+    try:
+        subprocess.check_call([
+            "git", "diff",
+            "--no-patch", "--exit-code",
+            "../model/models"
+        ])
+    except subprocess.CalledProcessError as err:
+        if err.returncode != 1:
+            raise
+        subprocess.call(['git', 'checkout', 'data/facts/'])
+        exit(1)
+
+if __name__ == '__main__':
+    main()
