3d0058a40eb703fd42767142833df44f09f44863
pinbraerts <shirshov.dima@gmail.com>
2017-11-18 23:02:39 +0300

    Added description

--- test/compareBuild.sh
+++ test/compareBuild.sh
@@ -2,5 +2,6 @@ find test/data/facts/ -type f -name "*.xml" -print0 | xargs -n1 -0 model/build
 
 git diff --no-patch --exit-code model/models || {
 	git checkout model/models
+	echo diff failed
 	exit 1
 }
