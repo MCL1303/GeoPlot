6a2d03bd6de6a6c2fae7b6b262fd7f877e20201d
pinbraerts <shirshov.dima@gmail.com>
2017-11-18 22:12:08 +0300

    Исправил ошибки

--- test/compare.sh
+++ test/compare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in test/input/*.txt; do
+for i in test/data/input/*.txt; do
 	extract/extract-normalize $i
 done
 
-git diff --no-patch --exit-code data/facts || git checkout data/facts
+git diff --no-patch --exit-code test/data/facts || git checkout test/data/facts
--- test/compareBuild.sh
+++ test/compareBuild.sh
@@ -1,3 +1,3 @@
 find test/data/facts/ -type f -name "*.xml" -print0 | xargs -n1 -0 model/build
 
-git diff --no-patch --exit-code data/facts || git checkout data/facts
+git diff --no-patch --exit-code data/facts || git checkout test/data/facts
