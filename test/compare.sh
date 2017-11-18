7e96d891c059eb89754575d302a49f9454d0fef0
pinbraerts <shirshov.dima@gmail.com>
2017-11-18 22:14:59 +0300

    Добавил выход с ошибкой

--- test/compare.sh
+++ test/compare.sh
@@ -4,4 +4,7 @@ for i in test/data/input/*.txt; do
 	extract/extract-normalize $i
 done
 
-git diff --no-patch --exit-code test/data/facts || git checkout test/data/facts
+git diff --no-patch --exit-code test/data/facts || {
+	git checkout test/data/facts
+	exit 1
+}
--- test/compareBuild.sh
+++ test/compareBuild.sh
@@ -1,3 +1,6 @@
 find test/data/facts/ -type f -name "*.xml" -print0 | xargs -n1 -0 model/build
 
-git diff --no-patch --exit-code data/facts || git checkout test/data/facts
+git diff --no-patch --exit-code data/facts || {
+	git checkout test/data/facts
+	exit 1
+}
