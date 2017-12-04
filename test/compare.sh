65996c78f748a85ae65505dcefff151cb7ac8b05
pinbraerts <pinbraerts@github.com>
2017-12-04 21:02:17 +0300

    removed exit

--- test/compare.sh
+++ test/compare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 
 for i in test/data/input/*.txt; do
 	extract/extract-normalize $i
 done
 
-git diff --exit-code test/data/facts || exit 1
+git diff --exit-code test/data/facts
--- test/compareBuild.sh
+++ test/compareBuild.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
 find test/data/facts/ -type f -name "*.xml" -print0 | xargs -n1 -0 model/build
 
-git diff --exit-code model/models || exit 1
+git diff --exit-code model/models
