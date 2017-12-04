803bc11be1cbd94cc05a9325cac2f6cd86b59a88
pinbraerts <shirshov.dima@gmail.com>
2017-12-04 18:46:51 +0300

    pycodestyle fixed

--- draw/draw.py
+++ draw/draw.py
@@ -8,30 +8,30 @@ from geometry import *
 
 
 def main():
-	model = json.load(sys.stdin)
-	for k, v in model.items():
-		try:
-			cls = classes()[k]
-			for item in v:
-				cls.load(item)
-		except KeyError:
-			raise TypeError("Error: Unknown object name '" + k + "'!")
+    model = json.load(sys.stdin)
+    for k, v in model.items():
+        try:
+            cls = classes()[k]
+            for item in v:
+                cls.load(item)
+        except KeyError:
+            raise TypeError("Error: Unknown object name '" + k + "'!")
 
-	HEIGHT = 1024
-	WIDTH = 1024
-	POINT_SIZE = 2
-	FONT = ImageFont.truetype('arial.ttf', 20)
+    HEIGHT = 1024
+    WIDTH = 1024
+    POINT_SIZE = 2
+    FONT = ImageFont.truetype('arial.ttf', 20)
 
-	out = Image.new('RGB', (HEIGHT, WIDTH), color=(255, 255, 255))
-	d = ImageDraw.Draw(out)
+    out = Image.new('RGB', (HEIGHT, WIDTH), color=(255, 255, 255))
+    d = ImageDraw.Draw(out)
 
-	d.height = HEIGHT
-	d.textFont = FONT
-	d.width = WIDTH
-	draw_all(d)
+    d.height = HEIGHT
+    d.textFont = FONT
+    d.width = WIDTH
+    draw_all(d)
 
-	out.save(sys.stdout.buffer, 'PNG')
+    out.save(sys.stdout.buffer, 'PNG')
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- draw/geometry/Segment.py
+++ draw/geometry/Segment.py
@@ -1,22 +1,26 @@
 from ._geomobj import _geomobj
 from .Point import Point
 
+
 class Segment(_geomobj):
-	def __init__(self, ends):
-		self.ends = ends
+    def __init__(self, ends):
+        self.ends = ends
 
-	instances = {}
+    instances = {}
 
-	@staticmethod
-	def load(dictionary):
-		return Segment.add((
-			Point.load(dictionary["first_end"]),
-			Point.load(dictionary["second_end"])
-		))
+    @staticmethod
+    def load(dictionary):
+        return Segment.add((
+            Point.load(dictionary["first_end"]),
+            Point.load(dictionary["second_end"])
+        ))
 
-	def render(self, d):
-		self.ends[0].draw(d)
-		self.ends[0].draw(d)
-		d.line((self.ends[0].x, self.ends[0].y,
-			self.ends[1].x, self.ends[1].y), (0, 0, 0), 3
-		)
+    def render(self, d):
+        self.ends[0].draw(d)
+        self.ends[0].draw(d)
+        d.line(
+            (
+                self.ends[0].x, self.ends[0].y,
+                self.ends[1].x, self.ends[1].y
+            ), (0, 0, 0), 3
+        )
