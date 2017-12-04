6c642d9f70c242548bea7b7c25c7bb498fc6d436
pinbraerts <shirshov.dima@gmail.com>
2017-12-04 18:26:18 +0300

    removed git checkout

--- test/compare.sh
+++ test/compare.sh
@@ -4,7 +4,4 @@ for i in test/data/input/*.txt; do
 	extract/extract-normalize $i
 done
 
-git diff --exit-code test/data/facts || {
-	git checkout test/data/facts
-	exit 1
-}
+git diff --exit-code test/data/facts || exit 1
