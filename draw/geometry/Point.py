9759690923cfe0d2f586a4892a375ebeb61e79d0
Dmitry <shirshov.dima@gmail.com>
2017-12-05 15:13:42 +0300

    pycodestyle fixed for draw

--- draw/geometry/Point.py
+++ draw/geometry/Point.py
@@ -1,28 +1,31 @@
 from ._geomobj import _geomobj
 
+
 class Point(_geomobj):
-	POINT_SIZE = 2
-	TEXT_OFFSET = 20
+    POINT_SIZE = 2
+    TEXT_OFFSET = 20
 
-	instances = {}
+    instances = {}
 
-	def __init__(self, name, x, y):
-		self.name = name
-		self.x = x
-		self.y = y
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
 
-	@staticmethod
-	def load(dictionary):
-		return Point.add(dictionary["name"], dictionary["x"], dictionary["y"])
+    @staticmethod
+    def load(dictionary):
+        return Point.add(dictionary["name"], dictionary["x"], dictionary["y"])
 
-	def render(self, d):
-		self.x *= d.width
-		self.y *= d.height
+    def render(self, d):
+        self.x *= d.width
+        self.y *= d.height
 
-		d.arc(
-			(self.x - self.POINT_SIZE, self.y - self.POINT_SIZE,
-				self.x + self.POINT_SIZE, self.y + self.POINT_SIZE),
-			0, 360, (0, 0, 0)
-		)
-		d.text((self.x - self.TEXT_OFFSET, self.y - self.TEXT_OFFSET),
-			self.name, (0, 0, 0), d.textFont)
+        d.arc(
+            (self.x - self.POINT_SIZE, self.y - self.POINT_SIZE,
+                self.x + self.POINT_SIZE, self.y + self.POINT_SIZE),
+            0, 360, (0, 0, 0)
+        )
+        d.text(
+            (self.x - self.TEXT_OFFSET, self.y - self.TEXT_OFFSET),
+            self.name, (0, 0, 0), d.textFont
+        )
--- draw/geometry/__init__.py
+++ draw/geometry/__init__.py
@@ -3,18 +3,21 @@ from os.path import realpath, dirname, join, basename, splitext
 
 __all__ = [
     splitext(basename(i))[0] for i in
-        iglob(join(dirname(realpath(__file__)), '*.py'))
+    iglob(join(dirname(realpath(__file__)), '*.py'))
     if not i.startswith('_')
 ]
 __all__ += ["classes", "draw_all"]
 
 __classes = None
+
+
 def classes():
     global __classes
     if __classes is not None:
         return __classes
-    __classes = { i.__name__: i for i in _geomobj._geomobj.__subclasses__() }
+    __classes = {i.__name__: i for i in _geomobj._geomobj.__subclasses__()}
     return __classes
+
 
 def draw_all(drawer):
     for cls in _geomobj._geomobj.__subclasses__():
